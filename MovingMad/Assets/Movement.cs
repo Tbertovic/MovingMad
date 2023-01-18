067f3b8a036bc1cdb3b3abbae223212af41bcc8d
Tbertovic <75337621+Tbertovic@users.noreply.github.com>
2023-01-17 20:24:58 -0500

    push

---
+++ MovingMad/Assets/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+    
+{
+    Rigidbody rigidbody;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+       
+            transform.position += (Vector3.left * speed) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.position += (Vector3.right * speed) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.position += (Vector3.up * speed) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.position += (Vector3.down * speed) * Time.deltaTime;
+        }
+    }
+}
