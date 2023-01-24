64b8f4799478908fe4f1c5d65bd0403b1a666ffd
Tbertovic <75337621+Tbertovic@users.noreply.github.com>
2023-01-23 20:29:07 -0500

    base collision/ camera / background

---
+++ MovingMad/Assets/Collisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    private void OnCollisionEnter2d(Collision2D collision)
+    {
+        if (collision.gameObject.name == "grass_sprite")
+        {
+            Debug.Log("you got hit");
+        }
+    }
+} 
